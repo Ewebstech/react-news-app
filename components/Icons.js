bbe7cf06d11a5adab4fb718905c974cf5f4653aa
Emmanuel Paul <emmanuel.paul@mypatrica.co>
2023-06-13 08:21:03 +0100

    Updates

--- components/Icons.js
+++ components/Icons.js
@@ -54,11 +54,11 @@ export function ChevronUpDownIcon({ h = "h-6" }) {
     <svg 
       xmlns="http://www.w3.org/2000/svg" 
       fill="none" viewBox="0 0 24 24" 
-      stroke-width="1.5" 
+      strokeWidth="1.5" 
       stroke="currentColor" 
       className={`${h} inline`}
     >
-      <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
     </svg>
   )
 }
